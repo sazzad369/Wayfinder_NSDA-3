8ca6deb639bba39fe4a42f8c8e8c96d5b3230df5
sazzad369 <mdsazzad0369@gmail.com>
2024-10-14 12:13:19 +0600

    activity created

---
+++ app/src/main/java/com/example/wayfinder_nsda_3/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.wayfinder_nsda_3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
